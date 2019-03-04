e4d2a5d1e1ba9c9334b3094f0184e4e709f5cbe0
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 20:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Jonny M';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
