10c9495d2d9f685cd8822727ae36b028fcef8b4a
Jm <jonathan.p.middleton@gmail.com>
2019-03-04 21:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 const currentUser = 'Jonny M';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
-currentUser.length;
-currentUser.slice(0,1);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}`;
+
